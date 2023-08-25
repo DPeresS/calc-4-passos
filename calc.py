da8a29c3ea2f3bc81bf62bd62a251396afc02d57
DPeresS <143039062+DPeresS@users.noreply.github.com>
2023-08-25 20:17:38 -0300

    Versão Soma + Subtração + Multiplicação ok1

--- calc.py
+++ calc.py
@@ -2,3 +2,4 @@ n1=int(input("Digite um número: "))
 n2=int(input("Digite outro número: "))
 print(n1, "+", n2, "=", n1+n2) ##Soma dos valores informados!
 print(n1, "-", n2, "=", n1-n2) ##Subtração dos valores informados!
+print(n1, "x", n2, "=", n1*n2) ##Multiplicação dos valores informados!
