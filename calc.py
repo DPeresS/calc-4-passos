73ddfb9c0084007fbcf94d084d07ce880a23158f
DPeresS <143039062+DPeresS@users.noreply.github.com>
2023-08-25 21:38:10 -0300

    Versão Soma + Subtração + Multiplicação + Divisão OK

--- calc.py
+++ calc.py
@@ -4,7 +4,7 @@ print(n1, "+", n2, "=", n1+n2) ##Soma dos valores informados!
 print(n1, "-", n2, "=", n1-n2) ##Subtração dos valores informados!
 print(n1, "x", n2, "=", n1*n2) ##Multiplicação dos valores informados!
 
-if n1==0 or n2==0:
+if n2==0:
     print("Não foi possível realizar a divisão por 0") 
 else:
     print(n1, "/", n2, "=", n1/n2) 
